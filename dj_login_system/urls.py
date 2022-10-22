d887c2ca6f556667e92b4c02c7efaabf82a37809
Rab-A-B <rab2@cesar.school>
2022-10-22 03:01:21 -0300

    trocando dj_login por we heite

--- dj_login_system/urls.py
+++ dj_login_system/urls.py
@@ -1,4 +1,4 @@
-"""dj_login_system URL Configuration
+"""we-heite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
